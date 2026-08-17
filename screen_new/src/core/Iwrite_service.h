1832e4df3e9c9cd1db7967446481fdef45583526
TimeBean <alexanderklush8@gmail.com>
2026-08-17 21:53:18 +0300

    refactor(screen): make write service interface purely abstract

--- screen_new/src/core/Iwrite_service.h
+++ screen_new/src/core/Iwrite_service.h
@@ -3,10 +3,15 @@
 class Iwrite_service
 {
 public:
-    virtual void print(const char *text) = 0;
+    virtual ~Iwrite_service() = default;
 
-    virtual void print(const String &text)
+    virtual void print_text(const char *text, unsigned int max_length = 0) = 0;
+    virtual void clear() = 0;
+
+    void print(const String &text, unsigned int max_length = 0)
     {
-        print(text.c_str());
+        print_text(text.c_str(), max_length);
     }
+
+    virtual void print_in_corner_of_4(const unsigned int corner_id, const float value, const char *metric) = 0;
 };
