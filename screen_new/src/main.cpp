e67c408a72214592be85f04f3c1c6503dabffedd
TimeBean <alexanderklush8@gmail.com>
2026-08-17 21:53:26 +0300

    feat(screen): blink LED and wrap incoming text on write

--- screen_new/src/main.cpp
+++ screen_new/src/main.cpp
@@ -3,26 +3,37 @@
 #include "secrets.h"
 #include "infrastructure/write_service.h"
 #include "infrastructure/http_service.h"
+#include "infrastructure/led_service.h"
+
+constexpr bool IS_DEBUG = false;
+constexpr unsigned long LED_ON_DURATION_MS = 200;
 
 write_service *writer;
 http_service *http;
+led_service *led;
 
 void setup()
 {
-	Serial.begin(115200);
+    Serial.begin(115200);
 
-	writer = new write_service(2, true);
+    writer = new write_service(2, IS_DEBUG, 16);
+    led = new led_service(BUILTIN_LED);
 
-	http = new http_service(WIFI_SSID, WIFI_PASSWORD, true, true);
-	http->init();
+    http = new http_service(WIFI_SSID, WIFI_PASSWORD, IS_DEBUG, IS_DEBUG);
+    http->init();
+
+    writer->clear();
 }
 
 void loop()
 {
-	String value = http->handle();
+    String value = http->handle();
 
-	if (value.length() > 0)
-	{
-		writer->print(value.c_str());
-	}
+    if (value.length() > 0)
+    {
+        writer->print_text(value.c_str(), 18);
+        led->blink(LED_ON_DURATION_MS);
+    }
+
+    led->update();
 }
